09b61b96193d8c0a22bdd392ad3122c4389914f5
basterdv <baster1@list.ru>
2024-02-04 16:36:04 +1000

    PostgresSQL и подключение базыданных и полноценнный поиск

--- E_Store/settings.py
+++ E_Store/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
 
     "debug_toolbar",
 
@@ -79,8 +80,12 @@ WSGI_APPLICATION = "E_Store.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "home",
+        "USER": "admin",
+        'PASSWORD': "admin",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
--- goods/utils.py
+++ goods/utils.py
@@ -1,3 +1,5 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+
 from goods.models import Products
 from django.db.models import Q
 
@@ -5,13 +7,24 @@ from django.db.models import Q
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    # 4 вариант полиска
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
+    return Products.objects.annotate(rank=SearchRank(vector,query)).order_by('-rank')
 
+    # 3 вариант поиска
+    # return Products.objects.annotate(search=SearchVector('name','description')).filter(search=query)
+
+    # 2 вариант поиска
+    return Products.objects.filter(description__search=query)
+
+    # 1 первый вариант поиска
     keywords = [word for word in query.split() if len(word) > 2]
 
-    q_objects = Q()
+# q_objects = Q()
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+# for token in keywords:
+#     q_objects |= Q(description__icontains=token)
+#     q_objects |= Q(name__icontains=token)
 
-    return Products.objects.filter(q_objects)
+# return Products.objects.filter(q_objects)
