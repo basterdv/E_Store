c4d3f0915c2261b9056f82c8d0b1fea31b85716a
basterdv <baster1@list.ru>
2024-02-04 21:30:48 +1000

    форма логина и пароля

---
+++ users/views.py
@@ -0,0 +1,42 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username,password=password)
+            if user:
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'title':'Home - Авторизация',
+        'form': form
+    }
+    return render(request,'users/login.html',context)
+
+def registration(request):
+    context = {
+        'title': 'Home - Регистрация'
+    }
+    return render(request, 'users/registration.html', context)
+
+def profile(request):
+    context = {
+        'title': 'Home - Кабинет'
+    }
+    return render(request, 'users/profile.html', context)
+
+def logout(request):
+    pass
