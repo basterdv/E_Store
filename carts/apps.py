76a9d85ede3f5f8d00a802b3f1161f8982228d1d
basterdv <baster1@list.ru>
2024-02-05 02:09:33 +1000

    форма корзины 2 этап

--- carts/apps.py
+++ carts/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class CartsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "carts"
+    verbose_name = 'Корзины'
+    verbose_name_plural = 'Корзина'
--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
-def cart_add(request, product_id):
+def cart_add(request, product_slug):
     pass
 
-def cart_change(request, product_id):
+def cart_change(request, product_slug):
     pass
 
-def cart_remove(request, product_id):
+def cart_remove(request, product_slug):
     pass
