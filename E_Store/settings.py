bffc2b0f9df45821cad8d280e859724a2fbb7c2a
basterdv <baster1@list.ru>
2024-02-04 18:42:23 +1000

    регистрация и профайл юзера

--- E_Store/settings.py
+++ E_Store/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = "E_Store.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
