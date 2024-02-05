3f9ed4ea3d049b33b7d3f10cb062e17ece64aacd
basterdv <baster1@list.ru>
2024-02-05 22:52:36 +1000

    форма корзины 4 этап

--- carts/views.py
+++ carts/views.py
@@ -5,25 +5,28 @@ from goods.models import Products
 
 
 def cart_add(request, product_slug):
-
     product = Products.objects.get(slug=product_slug)
 
     if request.user.is_authenticated:
-        carts = Cart.object.filter(user=request.user,product=product)
+        carts = Cart.object.filter(user=request.user, product=product)
 
         if carts.exists():
             cart = carts.first()
             if cart:
-                cart.quantity +=1
+                cart.quantity += 1
                 cart.save()
 
         else:
-            Cart.object.create(user=request.user,product=product,quantity=1)
+            Cart.object.create(user=request.user, product=product, quantity=1)
 
     return redirect(request.META['HTTP_REFERER'])
+
 
 def cart_change(request, product_slug):
     pass
 
-def cart_remove(request, product_slug):
-    pass
+
+def cart_remove(request, cart_id):
+    cart = Cart.object.get(id=cart_id)
+    cart.delete()
+    return redirect(request.META['HTTP_REFERER'])
