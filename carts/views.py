cce7a8b2c6a490273d8602a86df17b5f8a0a341a
basterdv <baster1@list.ru>
2024-02-06 23:41:33 +1000

    добавлен js script

--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,15 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
-
+from django.template.loader import render_to_string
 from carts.models import Cart
+from carts.utils import get_user_carts
 from goods.models import Products
 
 
-def cart_add(request, product_slug):
-    product = Products.objects.get(slug=product_slug)
+def cart_add(request):
+    product_id = request.POST.get('product_id')
+
+    product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.object.filter(user=request.user, product=product)
@@ -19,7 +23,21 @@ def cart_add(request, product_slug):
         else:
             Cart.object.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META['HTTP_REFERER'])
+    user_cart = get_user_carts(request)
+
+    cart_item_html = render_to_string(
+        'carts/includes/included_cart.html', {
+            'carts': user_cart
+        }, request=request
+    )
+
+    response_data = {
+        'message': 'Товар добавлен в корзину',
+        'cart_item_html': cart_item_html}
+
+    return JsonResponse(response_data)
+
+    # return redirect(request.META['HTTP_REFERER'])
 
 
 def cart_change(request, product_slug):
--- static/vassalage/js/jquery-ajax.js
+++ static/vassalage/js/jquery-ajax.js
@@ -1,84 +1,56 @@
 // Когда html документ готов (прорисован)
+
 $(document).ready(function () {
-    //Берём из разметки элемент по шв - оповещения от django
-    var notification = $('#notification');
-    // И через 7 сек. убираем
-    if (notification.length > 0){
-        setTimeout(function(){
-            notification.alert('close');
-        },7000);
-    }
-
-    // При клике по значку корзины открываем всплывающее(модальное) окно
-    $('#modalButton').click(function(){
-        $('#exampleModal').appendTo('body');
-
-        $('exampleModal').modal('show');
-    });
-
-    // Событие клик по кнопке закрыть окно корзины
-    $('#exampleModal').click(function(){
-        $('#exampleModal').modal('hide');
-    });
-
-    //Обработчик события радиокнопки выбора способа доставки
-    $('input[name='requires_delivery']').change(function(){
-        var selectValue === "1") {
-            $('#deliveryAddressField').show();
-        } else {
-            $('#deliveryAddressField').hide();
-        }
-    });
     // берем в переменную элемент разметки с id jq-notification для оповещений от ajax
-    //var successMessage = $("#jq-notification");
+    var successMessage = $("#jq-notification");
 
-    // // Ловим собыитие клика по кнопке добавить в корзину
-    // $(document).on("click", ".add-to-cart", function (e) {
-    //     // Блокируем его базовое действие
-    //     e.preventDefault();
+    // Ловим собыитие клика по кнопке добавить в корзину
+    $(document).on("click", ".add-to-cart", function (e) {
+        // Блокируем его базовое действие
+        e.preventDefault();
 
-    //     // Берем элемент счетчика в значке корзины и берем оттуда значение
-    //     var goodsInCartCount = $("#goods-in-cart-count");
-    //     var cartCount = parseInt(goodsInCartCount.text() || 0);
+        // Берем элемент счетчика в значке корзины и берем оттуда значение
+        var goodsInCartCount = $("#goods-in-cart-count");
+        var cartCount = parseInt(goodsInCartCount.text() || 0);
 
-    //     // Получаем id товара из атрибута data-product-id
-    //     var product_id = $(this).data("product-id");
+        // Получаем id товара из атрибута data-product-id
+        var product_id = $(this).data("product-id");
 
-    //     // Из атрибута href берем ссылку на контроллер django
-    //     var add_to_cart_url = $(this).attr("href");
+        // Из атрибута href берем ссылку на контроллер django
+        var add_to_cart_url = $(this).attr("href");
 
-    //     // делаем post запрос через ajax не перезагружая страницу
-    //     $.ajax({
-    //         type: "POST",
-    //         url: add_to_cart_url,
-    //         data: {
-    //             product_id: product_id,
-    //             csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
-    //         },
-    //         success: function (data) {
-    //             // Сообщение
-    //             successMessage.html(data.message);
-    //             successMessage.fadeIn(400);
-    //             // Через 7сек убираем сообщение
-    //             setTimeout(function () {
-    //                 successMessage.fadeOut(400);
-    //             }, 7000);
+        // делаем post запрос через ajax не перезагружая страницу
+        $.ajax({
+            type: "POST",
+            url: add_to_cart_url,
+            data: {
+                product_id: product_id,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
+            success: function (data) {
+                // Сообщение
+                successMessage.html(data.message);
+                successMessage.fadeIn(400);
+                // Через 7сек убираем сообщение
+                setTimeout(function () {
+                    successMessage.fadeOut(400);
+                }, 7000);
 
-    //             // Увеличиваем количество товаров в корзине (отрисовка в шаблоне)
-    //             cartCount++;
-    //             goodsInCartCount.text(cartCount);
+                // Увеличиваем количество товаров в корзине (отрисовка в шаблоне)
+                cartCount++;
+                goodsInCartCount.text(cartCount);
 
-    //             // Меняем содержимое корзины на ответ от django (новый отрисованный фрагмент разметки корзины)
-    //             var cartItemsContainer = $("#cart-items-container");
-    //             cartItemsContainer.html(data.cart_items_html);
+                // Меняем содержимое корзины на ответ от django (новый отрисованный фрагмент разметки корзины)
+                var cartItemsContainer = $("#cart-items-container");
+                cartItemsContainer.html(data.cart_items_html);
 
-    //         },
+            },
 
-    //         error: function (data) {
-    //             console.log("Ошибка при добавлении товара в корзину");
-    //         },
-    //     });
-    // });
+            error: function (data) {
+                console.log("Ошибка при добавлении товара в корзину");
+            },
+        });
+    });
 
 
 
@@ -207,4 +179,64 @@ $(document).ready(function () {
     //         },
     //     });
     // }
+// //Берём из разметки элемент по шв - оповещения от django
+    // var notification = $('#notification');
+    // // И через 7 сек. убираем
+    // if (notification.length > 0){
+    //     setTimeout(function(){
+    //         notification.alert('close');
+    //     },7000);
+    // }
+    //
+    // // При клике по значку корзины открываем всплывающее(модальное) окно
+    // $('#modalButton').click(function(){
+    //     $('#exampleModal').appendTo('body');
+    //
+    //     $('exampleModal').modal('show');
+    // });
+    //
+    // // Событие клик по кнопке закрыть окно корзины
+    // $('#exampleModal').click(function(){
+    //     $('#exampleModal').modal('hide');
+    // });
+    //
+    // //Обработчик события радиокнопки выбора способа доставки
+    // $('input[name='requires_delivery']').change(function(){
+    //     var selectValue === "1") {
+    //         $('#deliveryAddressField').show();
+    //     } else {
+    //         $('#deliveryAddressField').hide();
+    //     }
+    // });
+        // Берем из разметки элемент по id - оповещения от django
+    var notification = $('#notification');
+    // И через 7 сек. убираем
+    if (notification.length > 0) {
+        setTimeout(function () {
+            notification.alert('close');
+        }, 7000);
+    }
+
+    // При клике по значку корзины открываем всплывающее(модальное) окно
+    $('#modalButton').click(function () {
+        $('#exampleModal').appendTo('body');
+
+        $('#exampleModal').modal('show');
+    });
+
+    // Собыите клик по кнопке закрыть окна корзины
+    $('#exampleModal .btn-close').click(function () {
+        $('#exampleModal').modal('hide');
+    });
+
+    // Обработчик события радиокнопки выбора способа доставки
+    $("input[name='requires_delivery']").change(function() {
+        var selectedValue = $(this).val();
+        // Скрываем или отображаем input ввода адреса доставки
+        if (selectedValue === "1") {
+            $("#deliveryAddressField").show();
+        } else {
+            $("#deliveryAddressField").hide();
+        }
+    });
 });
