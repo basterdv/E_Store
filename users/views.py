b95d7234ff07001b0ab569bacee979ec87b188bd
basterdv <baster1@list.ru>
2024-02-07 23:14:09 +1000

    Добавлена корзина для не авторизированного пользователя

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+from carts.models import Cart
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 
@@ -15,9 +16,15 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request,f'{username}, Вы успешно вошли в аккаунт')
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
 
                 redirect_page = request.POST.get('next',None)
                 if redirect_page and redirect_page != reverse('user:logout'):
@@ -41,8 +48,15 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session.session_key
+
             user = form.instance
             auth.login(request, user)
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+
             messages.success(request, f'{user.username}, Вы успешно зарегистрировались')
             return HttpResponseRedirect(reverse('main:index'))
     else:
