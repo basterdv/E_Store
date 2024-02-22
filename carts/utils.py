bec532949f3fa85e44c33b15b82f40b69d3426c6
basterdv <baster1@list.ru>
2024-02-22 11:07:11 +1000

    Коммит перед выгрузкой на сервер

--- carts/utils.py
+++ carts/utils.py
@@ -3,8 +3,4 @@ from carts.models import Cart
 
 def get_user_carts(request):
     if request.user.is_authenticated:
-        return Cart.objects.filter(user=request.user).select_related('product')
-
-    if not request.session.session_key:
-        request.session.create()
-    return Cart.objects.filter(session_key=request.session.session_key).select_related('product')
+        return Cart.objects.filter(user=request.user)
